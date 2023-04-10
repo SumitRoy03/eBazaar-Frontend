1e9ebaa8b623b6e560357d98f1cd26bd463626ac
SumitRoy03 <rsumit647@gmail.com>
2023-04-10 23:08:58 +0530

    basic ui template done

---
+++ src/utils/temp.js
@@ -0,0 +1,28 @@
+import service from "../images/service-01.png"
+export const services = [
+  {
+    image: service,
+    title: "Free Shipping",
+    tagline: "From all orders above $5",
+  },
+  {
+    image: "images/service-02.png",
+    title: "Surprise Offers",
+    tagline: "Save upto 25%",
+  },
+  {
+    image: "images/service-03.png",
+    title: "Support 24/7",
+    tagline: "Shop with an expert",
+  },
+  {
+    image: "images/service-04.png",
+    title: "Affordable Prices",
+    tagline: "Get Factory Default Price",
+  },
+  {
+    image: "images/service-05.png",
+    title: "Secure Payments",
+    tagline: "100% Protected Payment",
+  },
+];
